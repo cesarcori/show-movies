89f07b021c93ea64830c9db0dcfdb3ac96dd1bc0
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-04-17 11:33:35 -0400

    Feat: Show list results on html

--- app.js
+++ app.js
@@ -8,5 +8,21 @@ fetch(url)
   .catch(error => console.log(error))
 
 let showData = (data) => {
-  console.log(data)
+  console.log(data.Search[0].Title)
+  let body = "";
+  for ( let i=0; i < data.Search.length; i++) {
+
+    body += `
+      
+      <div id="movie">
+        <p>Título <span class="text-result">${data.Search[i].Title}</span></p>
+        <p>Año <span  class="text-result">1000</span></p>
+        <p>Genero: <span  class="text-result">Action, Adventure</span></p>
+      </div>
+    `
+
+  };
+
+  document.getElementById("show").innerHTML = body;
+
 }
